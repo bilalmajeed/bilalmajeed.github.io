d79e22e38519aa856718c2dd9018e121fc7aa2ef
Bilal Majeed <bilalmajeed247@gmail.com>
2016-07-31 14:31:16 -0400

    added new project, changed bg, removed unused images, and other misc fixes

---
+++ js/projects.js
@@ -0,0 +1,25 @@
+var projects = [{
+    "title": "Handwritten Digits OCR Tool",
+    "desc": "A tool that recognizes handwritten digits, created to get a better understanding of OpenCV and machine learning libraries such as 'scikit-learn'",
+    "url": "https://github.com/bilalmajeed/digit_ocr"
+}, {
+    "title": "RateMyCar",
+    "desc": "A machine learning tool that rates your car based on statistical data",
+    "url": "https://github.com/bilalmajeed/RateMyCar"
+}, {
+    "title": "ECE2019",
+    "desc": "A landing page for the Electrical/Computer Engineering program at UW",
+    "url": "https://github.com/bilalmajeed/ece2019"
+}, {
+    "title": "Smart Switch",
+    "desc": "An app to control a custom made power switch using an Android device",
+    "url": "https://github.com/bilalmajeed/LED_Remote"
+}, {
+    "title" : "Bluetooth Controlled Car",
+    "desc" : "An Android app that controls an R/C Car modified to use an Arduino board paired with bluetooh modules",
+    "url" : "https://github.com/bilalmajeed/BluetoothCar"
+}, {
+    "title": "RPI Cloud Server",
+    "desc": "Online data storage created using a Raspberry Pi",
+    "url": "#"
+}];
